b7375200b1e216a61792301029c4759a623093a0
Loick Le Digabel <loick.ledigabel@gmail.com>
2026-04-11 18:42:08 +0200

    Update cursor movement on ghostty

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,3 +2,9 @@
 
 export PATH="$PATH:./node_modules/.bin"
 export EDITOR="code --wait"
+
+# Make Option+Arrow word movement behave like a text editor:
+# stop at the end of the current word, not the start of the next one.
+# Words = alphanumeric + underscore. Everything else is a boundary.
+autoload -U select-word-style
+select-word-style bash
